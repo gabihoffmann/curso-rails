ef8769b9e8bde16a8bebef11c294ffd57973c9b2
gabi <ah.gabriella@gmail.com>
2019-05-23 13:13:01 -0300

    Helpers
    Create and use helpers

--- crypto_wallet/app/helpers/application_helper.rb
+++ crypto_wallet/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    # https://apidock.com/ruby/DateTime/strftime
+    def dataBrasil(today)
+        today.strftime("%d/%m/%Y")
+    end
+
+    def nome_aplicacao
+        "Crypto Wallet Application"
+    end
 end
