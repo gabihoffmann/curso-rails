d3216e8014542db315ab73aa2024d33711edab5e
gabi <ah.gabriella@gmail.com>
2019-05-23 15:45:18 -0300

    Rails Routes, Environment
    Learn use rails resource e practice helpers to show rails environment

--- crypto_wallet/app/helpers/application_helper.rb
+++ crypto_wallet/app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
     def nome_aplicacao
         "Crypto Wallet Application"
     end
+
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Teste"
+        end
+    end
+        
 end
