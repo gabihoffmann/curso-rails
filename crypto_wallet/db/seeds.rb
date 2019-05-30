7c2a59baec0658860bd4e2c1afe844a929574a81
gabi <ah.gabriella@gmail.com>
2019-05-30 00:04:39 -0300

    Refactore task and seed

--- crypto_wallet/db/seeds.rb
+++ crypto_wallet/db/seeds.rb
@@ -5,33 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'tty-spinner'
 #Usanod o seeds para criar dados iniciais para o desenvolvimento da aplicação
-coin = Coin.create!(
+spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas...", format: :balloon_2)
+spinner.auto_spin
+
+coins = [
     {
         description: "Bitcoin",
         acronym: "BTC",
         url_image: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
 
-    }
-)
-puts "Criando moeda ..."
-coin = Coin.create!(
+    },
+
     {
         description: "DASH",
         acronym: "DASH",
         url_image: "https://media.dash.org/wp-content/uploads/Dash-D-white_on_blue_circle.png"
 
-    }
-)
-puts "Criando moeda ..."
-coin = Coin.create!(
+    },
+
     {
         description: "Ethereum",
         acronym: "ETH",
         url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/600px-ETHEREUM-YOUTUBE-PROFILE-PIC.png"
 
+    },
+
+    {
+        description: "Shazam",
+        acronym: "SZM",
+        url_image: "https://library.kissclipart.com/20180831/zte/kissclipart-shazam-symbol-png-clipart-captain-marvel-black-ada-770a336e34552609.png"
     }
-)
-puts "Criando moeda ..."
-puts "Moedas criadas com sucesso"
+    
+]
+
+coins.each do |coin|
+    sleep(0.5)
+    Coin.find_or_create_by!(coin)
+end
+
+spinner.stop("(Cadastrado com sucesso)") 
--- crypto_wallet/lib/tasks/dev.rake
+++ crypto_wallet/lib/tasks/dev.rake
@@ -1,9 +1,39 @@
 namespace :dev do
   desc "Configura o ambiente de desenvolvimento"
   task setup: :environment do
-    # ações da task
-    # %x() permite executar comandos do terminal
-    puts %x(rails db:drop db:create db:migrate db:seed)
+    # mantendo ação apenas no ambiente de desenvolvimento
+    if Rails.env.development?
+       # %x() permite executar comandos do terminal
+      show_spiner("Apagando BD...") do 
+        %x(rails db:drop)
+      end
+
+      show_spiner("Criando BD...") {%x(rails db:create)} 
+
+      show_spiner("Migrando BD...") do 
+        %x(rails db:migrate)
+      end
+      
+      show_spiner("Populando BD...") do 
+        %x(rails db:seed)
+      end
+
+    else 
+      puts "Você não está em ambiente de desenvolvimento!"
+    end
+  end
+
+  private
+
+  def show_spiner(msg_start ="Carregando",msg_end="Concluido com sucesso!")
+    spinner = TTY::Spinner.new("[:spinner] #{msg_start}")
+    spinner.auto_spin
+    # Bloco de Código: https://guru-sp.github.io/tutorial_ruby/blocos-ruby.html
+    yield
+    spinner.success("(#{msg_end})") # Stop animation
   end
 
 end
+
+
+
